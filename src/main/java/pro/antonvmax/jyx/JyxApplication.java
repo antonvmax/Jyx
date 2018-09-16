ca2330f339a24ad7e72a51115ad131373bedf37a
antonvmax <antonvmax@ya.ru>
2018-09-16 07:46:23 +0300

    Jyx :: Spring Boot, Maven + Web, Rest Repositories, Websocket, JPA, MySQL, H2

---
+++ src/main/java/pro/antonvmax/jyx/JyxApplication.java
@@ -0,0 +1,12 @@
+package pro.antonvmax.jyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyxApplication.class, args);
+	}
+}
